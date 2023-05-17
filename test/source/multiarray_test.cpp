d5863186e525f010e5ef03778fd8a510e009b8d0
kunzh <RimeK20@outlook.com>
2023-05-17 17:01:21 +0800

    fix bug: memory leak

--- test/source/multiarray_test.cpp
+++ test/source/multiarray_test.cpp
@@ -4,10 +4,10 @@
 
 void test_at() {
   ma::multiarray<int *, 3> a1;
-  a1.pointer = static_cast<int *>(malloc(sizeof(double) * 24));
   a1.set(static_cast<int *>(malloc(sizeof(int) * 24)), 2, 4, 3);
   std::cout << "size = " << a1.size() << "\n";
   assert(a1.size() == 24);
+  free(a1.pointer);
 }
 
 auto main() -> int {
